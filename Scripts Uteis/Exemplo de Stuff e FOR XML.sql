40f6e6ee4384e43fcd272eeb99b7b6ff12db7803
Lincoln Borges de Castro <lincolnborges@gmail.com>
2021-04-06 10:18:31 -0300

    Atualizações

--- Scripts Uteis/Exemplo de Stuff e FOR XML.sql
+++ Scripts Uteis/Exemplo de Stuff e FOR XML.sql
@@ -9,3 +9,12 @@ from	aa_SolicitacoesAnalisesProcedimentos sol
 where sol.numg_solicitacaoanalise =  3322925
 and   sol.numg_justificativa is not null
 group by numg_justificativa, numg_solicitacaoanalise
+
+
+
+	Select  stuff(',' + Cast(t.ID as varchar(9)), 1, 2, ' ') 
+	From   OSMTD.dbo.OSSYS_EMAIL_CONTENT  c 
+	Inner join OSMTD.dbo.OSSYS_TENANT t on c.TENANT_ID = t.ID
+	FOR XML path ('')
+
+
